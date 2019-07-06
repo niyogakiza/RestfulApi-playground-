d00d98eb9c7b33114cf99c7269c6fcd0ed3298ce
Aimable <aimable.niyogakiza@drpgroup.com>
2019-07-06 18:37:45 +0100

    Routing requests

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const server = http.createServer((req,res) => {
     const parsedUrl = url.parse(req.url,  true);
@@ -7,9 +8,48 @@ const server = http.createServer((req,res) => {
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
     const method = req.method.toLowerCase();
     const queryStringObject = parsedUrl.query;
-    res.end('I am a server\n');
-    console.log(`Request received on path: ${trimmedPath} \n with this method :  ${method} \n and the query : ${JSON.stringify(queryStringObject)}` )
+    const headers = req.headers;
+
+    // Get the payload if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data)
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        const chosenHandler = typeof(router[trimmedPath]) !== "undefined" ? router[trimmedPath] : handlers.notFound;
+        // send data to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
+        };
+
+        chosenHandler(data, (statusCode, payload) => {
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+            payload = typeof(payload) === 'object' ? payload : {};
+            const payloadToString = JSON.stringify(payload);
+            res.writeHead(statusCode);
+            res.end(payloadToString);
+            console.log(`Returning this response: ,
+             ${statusCode}, 
+             ${payloadToString}
+             `)
+        });
+    });
 });
+
+const handlers = {};
+handlers.sample = (data, callback) => callback(406, { 'name': 'Sample handler'});
+handlers.notFound = (data, callback) => callback(404);
+const router = {
+    'sample': handlers.sample
+};
 
 server.listen(3000,() => {
     console.log('Server Listening on port 3000')
