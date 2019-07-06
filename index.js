4f2d95ed77c5a62e6cc373036734e47209159573
Aimable <aimable.niyogakiza@drpgroup.com>
2019-07-06 17:16:07 +0100

    Setup eslint - prettier & method on output

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ const server = http.createServer((req,res) => {
     const parsedUrl = url.parse(req.url,  true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+    const method = req.method.toLowerCase();
     res.end('I am a server\n');
-    console.log(`Request received on path: ${trimmedPath}` )
+    console.log(`Request received on path: ${trimmedPath} with this method :  ${method}` )
 });
-
-
 
 server.listen(3000,() => {
-    console.log('I')
+    console.log('Server Listening on port 3000')
 });
-
 
 
 
