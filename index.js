20c7a6a649ba386aa15cd6671d5f57ce254ccd1c
Aimable <aimable.niyogakiza@drpgroup.com>
2019-07-06 17:28:54 +0100

    Parsing query string

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const server = http.createServer((req,res) => {
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
     const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
     res.end('I am a server\n');
-    console.log(`Request received on path: ${trimmedPath} with this method :  ${method}` )
+    console.log(`Request received on path: ${trimmedPath} \n with this method :  ${method} \n and the query : ${JSON.stringify(queryStringObject)}` )
 });
 
 server.listen(3000,() => {
